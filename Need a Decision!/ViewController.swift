7c80689f25c5653fd7a634404d78517822616d57
FischerAlx <fisheralx@icloud.com>
2022-11-06 18:55:33 +0100

    Initial Commit

---
+++ Need a Decision!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Need a Decision!
+//
+//  Created by Александр Фишер on 06.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
