dd209acbdd30e5850a31842c875f66c280236c18
FischerAlx <fisheralx@icloud.com>
2022-11-07 21:21:52 +0100

    change coin to the opposite with a button

--- Need a Decision!/ViewController.swift
+++ Need a Decision!/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var coinImageView: UIImageView!
+    var n: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        coinImageView.image = UIImage(imageLiteralResourceName: "coin3")
-        coinImageView.alpha = 0.9
         
     }
 
-
+    @IBAction func coinButtonPressed(_ sender: UIButton) {
+        if n % 2 == 1 {
+            coinImageView.image = UIImage(imageLiteralResourceName: "coin3")
+            n += 1
+        } else {
+            coinImageView.image = UIImage(imageLiteralResourceName: "coin1")
+            n += 1
+        }
+        coinImageView.alpha = 0.9
+        
+    }
+    
 }
 
