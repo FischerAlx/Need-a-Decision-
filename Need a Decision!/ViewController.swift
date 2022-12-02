e9546f6e1ea9063c35ed1dcdc9fd55bf87b8b954
FischerAlx <fisheralx@icloud.com>
2022-12-02 15:48:31 +0100

    add an animation for coin
    now you way more easily understand that coin was flipped with same result

--- Need a Decision!/ViewController.swift
+++ Need a Decision!/ViewController.swift
@@ -17,9 +17,21 @@ class ViewController: UIViewController {
        
         let coinArray = [UIImage(imageLiteralResourceName: "coin1"), UIImage(imageLiteralResourceName: "coin3")]
         
-        coinImageView.image = coinArray.randomElement()
-        coinImageView.alpha = 0.9
-        button.titleLabel?.text = "got it"
+        //coinImageView.image = coinArray.randomElement()
+        
+        UIView.animate(withDuration: 0.3) {
+                sender.alpha = 0.5
+                self.coinImageView.alpha = 0
+                //self.coinImageView.image = coinArray.randomElement()
+            }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+                self.coinImageView.alpha = 1.0
+                self.coinImageView.image = coinArray.randomElement()
+            } //UIView
+        }
         
     }
     
